32a9460878f006a9ccb28f48ab048b00765515a2
Rafa <rsbventures0@gmail.com>
2026-08-16 22:25:23 -0300

    Autoload GameState com var coins implementado

--- scripts/Coin.gd
+++ scripts/Coin.gd
@@ -6,8 +6,11 @@ signal collected(value: int)
 
 
 
+func _ready() -> void:
+	collected.connect(GameState.add_coins)
+
+
 func _on_body_entered(body: Node2D) -> void:
 	if body.is_in_group('player'):
-			collected.emit(value)
-			print('money!')
-			queue_free()
+		collected.emit(value)
+		queue_free()
--- scripts/autoloads/GameState.gd
+++ scripts/autoloads/GameState.gd
@@ -6,7 +6,7 @@ var coins: int = 0
 
 
 
-func add_coins(amount: int):
+func add_coins(amount: int) -> void:
 	if amount > 0:
 		coins += amount
 		coins_changed.emit(coins)
